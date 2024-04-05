cd4be51bd7be1024c1bbf64efe94d84d8a1fccce
COMP-142-TamandaMdyanyama <tmdyanyama@gmail.com>
2024-04-05 11:12:11 +0300

    added printMatrix function

--- Main.cpp
+++ Main.cpp
@@ -4,3 +4,62 @@
 * \date 5/04/2024
 * \copyright University of Nicosia
 */
+#include <iostream>
+using namespace std;
+
+//Global costants
+const int MAX_COL = 5;
+
+//Function Prototypes
+void printMatrix(const double mat[][MAX_COL], const int maxRow);
+
+/**
+* Shows the menu, user makes a selection.
+* <BR>
+* @return Returns 0
+*/
+int main() {
+
+	const int MAX_ROW = 5;
+	int choice;
+	double mat[MAX_ROW][MAX_COL] = { 0 };
+
+	do {
+		cout << "1) Fill with  random numbers " << endl;
+		cout << "2) Print Matrix " << endl;
+		cout << "8) Exit " << endl;
+		cout << " Please enter a choice " << endl;
+		cin >> choice;
+
+		switch (choice) {
+		case 1:
+			break;
+		case 2:
+			printMatrix(mat, MAX_ROW);
+			break;
+		case 8:
+			break;
+
+		}
+	} while (choice != 8);
+
+
+	return 0;
+}
+
+/**
+*The function prints the values of the matrix
+* @param mat The matrix to be printed
+* @param maxRow The number of rows of the matrix
+*/
+void printMatrix(const double mat[][MAX_COL], const int maxRow) {
+	cout << "Content of the matrix: " << endl;
+
+	for (int i = 0; i < maxRow; i++) {
+		cout << "\nR" << i << ": ";
+		for (int j = 0; j < MAX_COL; j++) {
+			cout << mat[i][j] << "\t";  //@todo use setw to make it neater
+		}
+	}
+	cout << endl;
+}
